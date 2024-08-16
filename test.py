b9ee33831f7289080b922d467f1fd12b08f22714
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-08-16 14:07:21 +0300

    Add test file

---
+++ test.py
@@ -0,0 +1,6 @@
+import winaccent
+
+print(f"Accent color based on theme: {winaccent.accent_auto}")
+print(f"Light mode accent color: {winaccent.accent_light}")
+print(f"Dark mode accent color: {winaccent.accent_dark}")
+print(f"Normal accent color: {winaccent.accent_normal}")
