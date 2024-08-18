1b9db432f81430e682d686029ecbaa73240542c2
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-08-18 17:43:27 +0300

    Update the demos

--- demo_console.py
+++ demo_console.py
@@ -3,7 +3,6 @@ import winaccent
 print("\nwinaccent demo program")
 print("======================\n")
 
-print(f"Accent color based on theme: {winaccent.accent_auto}")
 print(f"Light mode accent color: {winaccent.accent_light}")
 print(f"Dark mode accent color: {winaccent.accent_dark}")
 print(f"Normal accent color: {winaccent.accent_normal}")
--- demo_gui.py
+++ demo_gui.py
@@ -1,9 +1,13 @@
 import tkinter as tk, winaccent
+from tkinter import ttk
 
 window = tk.Tk()
 window.title("winaccent demo program")
 window.resizable(False, False)
 window.configure(padx = 8, pady = 4)
+
+style = ttk.Style()
+style.configure("TButton", font = 11)
 
 def add_item(color, text):
     color_item = tk.Frame(window, padx = 10, pady = 8)
@@ -12,9 +16,16 @@ def add_item(color, text):
     tk.Frame(color_item, width = 30, height = 30, bg = color, highlightthickness = 1, highlightbackground = "#000000").pack(side = "left")
     tk.Label(color_item, text = f"{text}: {color}", font = 11).pack(side = "left", padx = (8, 0))
 
-add_item(winaccent.accent_auto, "Accent color based on theme")
-add_item(winaccent.accent_light, "Light mode accent color")
-add_item(winaccent.accent_dark, "Dark mode accent color")
-add_item(winaccent.accent_normal, "Normal accent color")
+def update_accent_colors():
+    winaccent.update_accent_colors()
+    for widget in window.winfo_children(): widget.destroy()
+
+    add_item(winaccent.accent_light, "Light mode accent color")
+    add_item(winaccent.accent_dark, "Dark mode accent color")
+    add_item(winaccent.accent_normal, "Normal accent color")
+
+    ttk.Button(window, text = "Refresh", command = update_accent_colors, default = "active").pack(fill = "x", pady = 8, padx = 5)
+
+update_accent_colors()
 
 window.mainloop()
