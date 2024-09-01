2bb760dd12a52ae42f313fa30c17f5291c9e88c5
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-09-01 18:01:59 +0300

    Show the demo when running `python -m winaccent`

--- winaccent/__main__.py
+++ winaccent/__main__.py
@@ -1,5 +1,21 @@
-import tkinter as tk, winaccent, threading
-from tkinter import ttk
+import winaccent, threading
+
+try:
+    import tkinter as tk
+    from tkinter import ttk
+except:
+    # If tkinter is unavailable, print the colors directly
+
+    print("\nAccent color palette")
+    print("======================\n")
+    
+    print(f"accent_light_3:    {winaccent.accent_light_3}")
+    print(f"accent_light_2:    {winaccent.accent_light_2}")
+    print(f"accent_light_1:    {winaccent.accent_light_1}")
+    print(f"accent_normal:     {winaccent.accent_normal}")
+    print(f"accent_dark_1:     {winaccent.accent_dark_1}")
+    print(f"accent_dark_2:     {winaccent.accent_dark_2}")
+    print(f"accent_dark_3:     {winaccent.accent_dark_3}")
 
 window = tk.Tk()
 window.title("Accent color palette")
