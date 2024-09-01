d0cfbd81ab8240e1f5052488ed89009d92d1978b
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-09-01 18:25:17 +0300

    Bump to 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ description = open("README.md", "r", encoding = "utf8").read()
 
 setup(
     name = "winaccent",
-    version = "0.3.0",
+    version = "1.0.0",
     license = "MIT",
     author = "Valer100",
     description = "A simple module for getting Windows' accent color",
