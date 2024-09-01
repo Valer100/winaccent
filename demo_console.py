0e326ccf36d2d635c2ea2b2d750e24d15cd9f431
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-09-01 17:59:07 +0300

    Update demos

--- demo_console.py
+++ demo_console.py
@@ -3,6 +3,10 @@ import winaccent
 print("\nwinaccent demo program")
 print("======================\n")
 
-print(f"Light mode accent color: {winaccent.accent_light}")
-print(f"Dark mode accent color: {winaccent.accent_dark}")
-print(f"Normal accent color: {winaccent.accent_normal}")
+print(f"accent_light_3:    {winaccent.accent_light_3}")
+print(f"accent_light_2:    {winaccent.accent_light_2}")
+print(f"accent_light_1:    {winaccent.accent_light_1}")
+print(f"accent_normal:     {winaccent.accent_normal}")
+print(f"accent_dark_1:     {winaccent.accent_dark_1}")
+print(f"accent_dark_2:     {winaccent.accent_dark_2}")
+print(f"accent_dark_3:     {winaccent.accent_dark_3}")
--- demo_gui.py
+++ demo_gui.py
@@ -2,27 +2,31 @@ import tkinter as tk, winaccent, threading
 from tkinter import ttk
 
 window = tk.Tk()
-window.title("winaccent demo program")
+window.title("Accent color palette")
 window.resizable(False, False)
-window.configure(padx = 8, pady = 4)
+window.configure(padx = 8, pady = 8)
 
 style = ttk.Style()
 style.configure("TButton", font = 11)
 
 def add_item(color, text):
-    color_item = tk.Frame(window, padx = 10, pady = 8)
-    color_item.pack(pady = 4, anchor = "w")
+    color_item = tk.Frame(window, padx = 8, pady = 0)
+    color_item.pack(pady = 2, anchor = "w")
 
-    tk.Frame(color_item, width = 30, height = 30, bg = color, highlightthickness = 1, highlightbackground = "#000000").pack(side = "left")
-    tk.Label(color_item, text = f"{text}: {color}", font = 11).pack(side = "left", padx = (8, 0))
+    tk.Frame(color_item, width = 20, height = 20, bg = color, highlightthickness = 1, highlightbackground = "#000000").pack(side = "left")
+    tk.Label(color_item, text = f"{text}:          {color}", font = ("Default", 11)).pack(side = "left", padx = (8, 0))
 
 def update_accent_colors():
     winaccent.update_accent_colors()
     for widget in window.winfo_children(): widget.destroy()
 
-    add_item(winaccent.accent_light, "Light mode accent color")
-    add_item(winaccent.accent_dark, "Dark mode accent color")
-    add_item(winaccent.accent_normal, "Normal accent color")
+    add_item(winaccent.accent_light_3, "accent_light_3")
+    add_item(winaccent.accent_light_2, "accent_light_2")
+    add_item(winaccent.accent_light_1, "accent_light_1")
+    add_item(winaccent.accent_normal, "accent_normal")
+    add_item(winaccent.accent_dark_1, "accent_dark_1")
+    add_item(winaccent.accent_dark_2, "accent_dark_2")
+    add_item(winaccent.accent_dark_3, "accent_dark_3")
 
 update_accent_colors()
 
