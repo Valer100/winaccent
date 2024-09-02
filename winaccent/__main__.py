397fc0e7a8b153f91174d3006e4fb11f5ede08f2
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-09-02 20:14:31 +0300

    Don't run tkinter code if tkinter is unavailable

--- winaccent/__main__.py
+++ winaccent/__main__.py
@@ -3,9 +3,53 @@ import winaccent, threading
 try:
     import tkinter as tk, ctypes
     from tkinter import ttk
+    
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("winaccent.demo")
+
+    window = tk.Tk()
+    window.title("Accent palette")
+    window.resizable(False, False)
+    window.configure(padx = 8, pady = 8)
+    
+    icon = tk.PhotoImage(data = "iVBORw0KGgoAAAANSUhEUgAAAEwAAABMBAMAAAA1uUwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAYUExURQAAAAAaaAA+kgBnwAB41ACR+EzC/5nr/8MyyRkAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAA9SURBVEjH7coxAQAQFEXRLwIRiEAEIhCB1aa+l4LlnvnYH85LiJKylCqty5iyjmwajUaj0Wg0Gu19e87sAnxWiuenyOclAAAAAElFTkSuQmCC")
+    window.iconphoto(True, icon)
+    
+    style = ttk.Style()
+    style.configure("TButton", font = 11)
+    
+    def add_item(color, text):
+        color_item = tk.Frame(window, padx = 8, pady = 0)
+        color_item.pack(pady = 2, anchor = "w")
+    
+        tk.Frame(color_item, width = 20, height = 20, bg = color, highlightthickness = 1, highlightbackground = "#000000").pack(side = "left")
+        ttk.Label(color_item, text = text, font = ("Default", 11), width = 17).pack(side = "left", padx = (8, 0), anchor = "w")
+        
+        color_value = tk.Text(color_item, width = 7, height = 1, font = ("Consolas", 11), bd = 0, bg = ttk.Style().lookup(".", "background"))
+        color_value.pack(side = "right")
+        color_value.insert("1.0", color)
+        color_value["state"] = "disabled"
+    
+    def update_accent_colors():
+        winaccent.update_accent_colors()
+        for widget in window.winfo_children(): widget.destroy()
+    
+        add_item(winaccent.accent_light_3, "accent_light_3")
+        add_item(winaccent.accent_light_2, "accent_light_2")
+        add_item(winaccent.accent_light_1, "accent_light_1")
+        add_item(winaccent.accent_normal, "accent_normal")
+        add_item(winaccent.accent_dark_1, "accent_dark_1")
+        add_item(winaccent.accent_dark_2, "accent_dark_2")
+        add_item(winaccent.accent_dark_3, "accent_dark_3")
+    
+    update_accent_colors()
+    
+    thread = threading.Thread(target = lambda: winaccent.on_accent_changed_listener(update_accent_colors), daemon = True)
+    thread.start()
+    
+    window.mainloop()
 except:
     # tkinter is unavailable, print the colors directly
-    print("tkinter isn't available. Perhaps it isn't installed or the installation is corrupted.\n")
+    print("tkinter isn't available. Perhaps it isn't installed or the installation is corrupted.")
 
     print("\nAccent palette")
     print("================\n")
@@ -17,47 +61,3 @@ except:
     print(f"accent_dark_1:     {winaccent.accent_dark_1}")
     print(f"accent_dark_2:     {winaccent.accent_dark_2}")
     print(f"accent_dark_3:     {winaccent.accent_dark_3}")
-
-ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("winaccent.demo")
-
-window = tk.Tk()
-window.title("Accent palette")
-window.resizable(False, False)
-window.configure(padx = 8, pady = 8)
-
-icon = tk.PhotoImage(data = "iVBORw0KGgoAAAANSUhEUgAAAEwAAABMBAMAAAA1uUwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAYUExURQAAAAAaaAA+kgBnwAB41ACR+EzC/5nr/8MyyRkAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAA9SURBVEjH7coxAQAQFEXRLwIRiEAEIhCB1aa+l4LlnvnYH85LiJKylCqty5iyjmwajUaj0Wg0Gu19e87sAnxWiuenyOclAAAAAElFTkSuQmCC")
-window.iconphoto(True, icon)
-
-style = ttk.Style()
-style.configure("TButton", font = 11)
-
-def add_item(color, text):
-    color_item = tk.Frame(window, padx = 8, pady = 0)
-    color_item.pack(pady = 2, anchor = "w")
-
-    tk.Frame(color_item, width = 20, height = 20, bg = color, highlightthickness = 1, highlightbackground = "#000000").pack(side = "left")
-    ttk.Label(color_item, text = text, font = ("Default", 11), width = 17).pack(side = "left", padx = (8, 0), anchor = "w")
-    
-    color_value = tk.Text(color_item, width = 7, height = 1, font = ("Consolas", 11), bd = 0, bg = ttk.Style().lookup(".", "background"))
-    color_value.pack(side = "right")
-    color_value.insert("1.0", color)
-    color_value["state"] = "disabled"
-
-def update_accent_colors():
-    winaccent.update_accent_colors()
-    for widget in window.winfo_children(): widget.destroy()
-
-    add_item(winaccent.accent_light_3, "accent_light_3")
-    add_item(winaccent.accent_light_2, "accent_light_2")
-    add_item(winaccent.accent_light_1, "accent_light_1")
-    add_item(winaccent.accent_normal, "accent_normal")
-    add_item(winaccent.accent_dark_1, "accent_dark_1")
-    add_item(winaccent.accent_dark_2, "accent_dark_2")
-    add_item(winaccent.accent_dark_3, "accent_dark_3")
-
-update_accent_colors()
-
-thread = threading.Thread(target = lambda: winaccent.on_accent_changed_listener(update_accent_colors), daemon = True)
-thread.start()
-
-window.mainloop()
