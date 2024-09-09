5d93d0c0d4a453e5da51d488791b2086418b8556
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-09-09 15:31:49 +0300

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ description = open("README.md", "r", encoding = "utf8").read()
 
 setup(
     name = "winaccent",
-    version = "1.0.0",
+    version = "1.1.0",
     license = "MIT",
     author = "Valer100",
     maintainer = "Valer100",
