b585030029766b03fbab72e1623d54c96820aaf9
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-09-26 18:36:45 +0300

    Replicate more Windows behavior

--- winaccent/__init__.py
+++ winaccent/__init__.py
@@ -44,7 +44,8 @@ def update_accent_colors():
     accent_dark_2 = "#" + accent_palette_list[20] + accent_palette_list[21] + accent_palette_list[22]
     accent_dark_3 = "#" + accent_palette_list[24] + accent_palette_list[25] + accent_palette_list[26]
     
-    accent_menu = _get_color_from_registry_rgb(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Accent", "AccentColorMenu", "abgr")
+    try: accent_menu = _get_color_from_registry_rgb(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Accent", "AccentColorMenu", "abgr")
+    except: accent_menu = accent_normal
 
     accent_dark = accent_dark_1
     accent_light = accent_light_2
@@ -62,6 +63,7 @@ def update_accent_colors():
     # Replicate Windows' behavior if titlebar_active, titlebar_inactive and window_border are set to 0
     if titlebar_active == "0": titlebar_active = accent_menu
     if titlebar_inactive == "0": titlebar_inactive = accent_menu
+    if accent_menu == "0": accent_menu = accent_normal
     if window_border == "0": window_border = "#000000"
 
     is_titlebar_colored = _get_registry_value(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\DWM", "ColorPrevalence")
