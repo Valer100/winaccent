5dda431859e6d743f7468852ac7e01b075925c59
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-10-03 16:47:52 +0300

    Some changes to get the right titlebar color

--- winaccent/win_8.py
+++ winaccent/win_8.py
@@ -30,7 +30,21 @@ def update_accent_colors():
     accent_normal = utils.get_color_from_registry_rgb(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Accent", "AccentColor", "abgr")
     accent_menu = utils.get_color_from_registry_rgb(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Accent", "StartColor", "abgr")
 
-    titlebar_active = utils.get_color_from_registry_rgb(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\DWM", "ColorizationColor", "argb")
+    titlebar_active_max_intensity = utils.get_color_from_registry_rgb(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\DWM", "ColorizationColor", "argb")
+    
+    titlebar_active_intensity = utils.get_registry_value(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\DWM", "ColorizationColorBalance")
+    titlebar_active_intensity = 255 * titlebar_active_intensity / 100
+
+    red = int(titlebar_active_max_intensity[1] + titlebar_active_max_intensity[2], base = 16)
+    green = int(titlebar_active_max_intensity[3] + titlebar_active_max_intensity[4], base = 16)
+    blue = int(titlebar_active_max_intensity[5] + titlebar_active_max_intensity[6], base = 16)
+
+    red = (((red * titlebar_active_intensity)) + (0xD9 * (255 - titlebar_active_intensity))) / 255
+    green = (((green * titlebar_active_intensity)) + (0xD9 * (255 - titlebar_active_intensity))) / 255
+    blue = (((blue * titlebar_active_intensity)) + (0xD9 * (255 - titlebar_active_intensity))) / 255
+
+    titlebar_active = f"{round(red):02X}{round(green):02X}{round(blue):02X}"
+
     titlebar_inactive = "#EBEBEB"
     window_border = titlebar_active
 
