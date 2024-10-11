326e1d919893a38ebc76966080eaf8bbb245c843
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-10-11 22:56:56 +0300

    Add fallback values if errors occur

--- winaccent/win_10.py
+++ winaccent/win_10.py
@@ -24,19 +24,38 @@ def update_accent_colors():
     global accent_menu
     global is_accent_palette_supported
 
-    accent_palette = utils.get_registry_value(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Accent", "AccentPalette")
-    accent_palette = " ".join(f'{byte:02X}' for byte in accent_palette)
-    accent_palette_list = accent_palette.split(" ")
+    try:
+        accent_palette = utils.get_registry_value(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Accent", "AccentPalette")
+        accent_palette = " ".join(f'{byte:02X}' for byte in accent_palette)
+        accent_palette_list = accent_palette.split(" ")
 
-    # Guidelines: https://learn.microsoft.com/en-us/windows/apps/design/style/color#accent-color-palette
-    accent_light_3 = "#" + accent_palette_list[0] + accent_palette_list[1] + accent_palette_list[2]
-    accent_light_2 = "#" + accent_palette_list[4] + accent_palette_list[5] + accent_palette_list[6]
-    accent_light_1 = "#" + accent_palette_list[8] + accent_palette_list[9] + accent_palette_list[10]
-    accent_normal = "#" + accent_palette_list[12] + accent_palette_list[13] + accent_palette_list[14]
-    accent_dark_1 = "#" + accent_palette_list[16] + accent_palette_list[17] + accent_palette_list[18]
-    accent_dark_2 = "#" + accent_palette_list[20] + accent_palette_list[21] + accent_palette_list[22]
-    accent_dark_3 = "#" + accent_palette_list[24] + accent_palette_list[25] + accent_palette_list[26]
-    
+        # Guidelines: https://learn.microsoft.com/en-us/windows/apps/design/style/color#accent-color-palette
+        accent_light_3 = "#" + accent_palette_list[0] + accent_palette_list[1] + accent_palette_list[2]
+        accent_light_2 = "#" + accent_palette_list[4] + accent_palette_list[5] + accent_palette_list[6]
+        accent_light_1 = "#" + accent_palette_list[8] + accent_palette_list[9] + accent_palette_list[10]
+        accent_normal = "#" + accent_palette_list[12] + accent_palette_list[13] + accent_palette_list[14]
+        accent_dark_1 = "#" + accent_palette_list[16] + accent_palette_list[17] + accent_palette_list[18]
+        accent_dark_2 = "#" + accent_palette_list[20] + accent_palette_list[21] + accent_palette_list[22]
+        accent_dark_3 = "#" + accent_palette_list[24] + accent_palette_list[25] + accent_palette_list[26]
+    except:
+        if sys.getwindowsversion().major == 10 and sys.getwindowsversion().build >= 22000:
+            accent_light_3 = "#99EBFF"
+            accent_light_2 = "#4CC2FF"
+            accent_light_1 = "#0091F8"
+            accent_normal = "#0078D4"
+            accent_dark_1 = "#0067C0"
+            accent_dark_2 = "#003E92"
+            accent_dark_3 = "#001A68"
+        elif sys.getwindowsversion().major == 10:
+            accent_light_3 = "#A6D8FF"
+            accent_light_2 = "#76B9ED"
+            accent_light_1 = "#429CE3"
+            accent_normal = "#0078D7"
+            accent_dark_1 = "#005A9E"
+            accent_dark_2 = "#004275"
+            accent_dark_3 = "#002642"
+
+
     try: accent_menu = utils.get_color_from_registry_rgb(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Accent", "AccentColorMenu", "abgr")
     except: accent_menu = accent_normal
 
