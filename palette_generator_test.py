0576ea6fc4725e54f1cfcde045329bdd5a9029c4
Valer100 <122545522+Valer100@users.noreply.github.com>
2024-11-19 18:58:39 +0200

    Follow the new GUI design

--- palette_generator_test.py
+++ palette_generator_test.py
@@ -1,15 +1,15 @@
-import tkinter as tk, ctypes
+import tkinter as tk, ctypes, winaccent
 from tkinter import ttk, colorchooser
-from winaccent import _utils
     
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("winaccent.demo")
 
 window = tk.Tk()
 window.title("Palette generator")
 window.resizable(False, False)
-window.configure(padx = 8, pady = 8)
+window.configure(padx = 10, pady = 10)
 
 accent_normal = "#4617B4"
+accent_dark_3 = winaccent._utils.generate_color_palette(accent_normal)[5]
 
 icon = tk.PhotoImage(data = "iVBORw0KGgoAAAANSUhEUgAAAEwAAABMBAMAAAA1uUwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAYUExURQAAAAAaaAA+kgBnwAB41ACR+EzC/5nr/8MyyRkAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAA9SURBVEjH7coxAQAQFEXRLwIRiEAEIhCB1aa+l4LlnvnYH85LiJKylCqty5iyjmwajUaj0Wg0Gu19e87sAnxWiuenyOclAAAAAElFTkSuQmCC")
 window.iconphoto(True, icon)
@@ -20,9 +20,9 @@ style.configure("TButton", font = 11)
 ttk.Label(window, text = "Main color", font = ("Segoe UI Semibold", 15)).pack(padx = 8, pady = (0, 8), anchor = "w")
 
 color = ttk.Frame(window)
-color.pack(fill = "x", anchor = "w", padx = 8)
+color.pack(fill = "x", anchor = "w", padx = 10)
 
-color_prev = tk.Frame(color, width = 20, height = 20, highlightthickness = 1, highlightbackground = "SystemButtonText", bg = accent_normal)
+color_prev = tk.Frame(color, width = 23, height = 23, highlightthickness = 1, highlightbackground = accent_dark_3, bg = accent_normal)
 color_prev.pack(side = "left")
 
 def on_color_change(event = None):
@@ -52,28 +52,29 @@ color_input.bind("<KeyRelease>", on_color_change)
 
 pick_color = ttk.Button(color, text = "...", width = 3, command = choose_color).pack(side = "left")
 
+ttk.Frame(window, width = 280).pack()
+
 palette = ttk.Frame(window)
-palette.pack(anchor = "w")
+palette.pack(anchor = "w", fill = "x")
 
-def add_item(color, text):
-    color_item = tk.Frame(palette, padx = 8, pady = 0)
-    color_item.pack(pady = 2, anchor = "w")
+def add_item(parent, color, text):
+    text_color = "#FFFFFF" if winaccent._utils.white_text_on_color(color) else "#000000"
+    text_color_inverse = "#000000" if winaccent._utils.white_text_on_color(color) else "#FFFFFF"
 
-    color_prev = tk.Frame(color_item, width = 20, height = 20, highlightthickness = 1, highlightbackground = "SystemButtonFace")
-    color_prev.pack(side = "left")
+    color_item = tk.Frame(parent, bg = color, padx = 8, pady = 8)
+    color_item.pack(fill = "x")
 
-    try: color_prev.configure(bg = color, highlightbackground = "SystemButtonText")
-    except: pass
-    
-    ttk.Label(color_item, text = text, font = ("Default", 11), width = 23).pack(side = "left", padx = (8, 0), anchor = "w")
-    
-    color_value = tk.Text(color_item, width = 7, height = 1, font = ("Consolas", 11), bd = 0, bg = ttk.Style().lookup(".", "background"))
+    color_name = tk.Label(color_item, text = text, font = ("Default", 11), fg = text_color, bg = color)
+    color_name.pack(side = "left")
+
+    color_value = tk.Text(color_item, width = 7, height = 1, font = ("Consolas", 11), bd = 0, bg = color, fg = text_color, 
+                          selectbackground = text_color, selectforeground = text_color_inverse)
     color_value.pack(side = "right")
     color_value.insert("1.0", str(color))
     color_value["state"] = "disabled"
 
 def update_palette():
-    accent_palette = _utils.generate_color_palette(accent_normal)
+    accent_palette = winaccent._utils.generate_color_palette(accent_normal)
 
     accent_light_3 = accent_palette[0]
     accent_light_2 = accent_palette[1]
@@ -85,13 +86,16 @@ def update_palette():
     for widget in palette.winfo_children(): widget.destroy()
     ttk.Label(palette, text = "Generated palette", font = ("Segoe UI Semibold", 15)).pack(padx = 8, pady = (16, 8), anchor = "w")
 
-    add_item(accent_light_3, "accent_light_3")
-    add_item(accent_light_2, "accent_light_2")
-    add_item(accent_light_1, "accent_light_1")
-    add_item(accent_normal, "accent_normal")
-    add_item(accent_dark_1, "accent_dark_1")
-    add_item(accent_dark_2, "accent_dark_2")
-    add_item(accent_dark_3, "accent_dark_3")
+    palette_colors = tk.Frame(palette, highlightbackground = accent_dark_3, highlightthickness = 1)
+    palette_colors.pack(anchor = "w", fill = "x", padx = 8, pady = (0, 8))
+
+    add_item(palette_colors, accent_light_3, "accent_light_3")
+    add_item(palette_colors, accent_light_2, "accent_light_2")
+    add_item(palette_colors, accent_light_1, "accent_light_1")
+    add_item(palette_colors, accent_normal, "accent_normal")
+    add_item(palette_colors, accent_dark_1, "accent_dark_1")
+    add_item(palette_colors, accent_dark_2, "accent_dark_2")
+    add_item(palette_colors, accent_dark_3, "accent_dark_3")
 
     window.update()
     window.geometry(window.geometry())
